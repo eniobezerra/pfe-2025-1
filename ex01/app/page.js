a184f0876b884d8da278237b8aef29c19febc711
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2025-02-14 00:38:03 +0000

    dia 130225

---
+++ ex01/app/page.js
@@ -0,0 +1,14 @@
+export default function Home(){
+  //pra colocar mais de um linha usa div dentro do parentese
+  // ou <> <> fragmento
+  const aula = 2;
+    return(
+    <div>
+
+     <h1> Ola turma </h1>
+
+     <p> essa é a nossa  primeira {aula} ª de react</p>
+    </div>
+    )
+  
+}
